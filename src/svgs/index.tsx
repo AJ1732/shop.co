72462897ad68a40d5820eac2ee78c2c610fa1b5d
AJ1732 <ejemeniboi@gmail.com>
2024-12-25 00:24:03 +0100

    chore: move folders

--- src/svgs/index.tsx
+++ src/svgs/index.tsx
@@ -2,4 +2,5 @@ export { default as ApplePaySVG } from "./ApplePaySVG";
 export { default as GooglePaySVG } from "./GooglePaySVG";
 export { default as MastercardSVG } from "./MastercardSVG";
 export { default as PayPalSVG } from "./PayPalSVG";
+export { default as StarSVG } from "./StarSVG";
 export { default as VisaSVG } from "./VisaSVG";
