e724649bc1d72ff9040f6d16ffc548907cfaeb05
AJ1732 <ejemeniboi@gmail.com>
2024-12-26 12:55:41 +0100

    chore: import section skeleton

--- src/app/(pages)/shop/[shopId]/page.tsx
+++ src/app/(pages)/shop/[shopId]/page.tsx
@@ -6,26 +6,29 @@ import {
   ProductDisplaySection,
   ProductTabSection,
 } from "@/components/sections/ShopIdPage";
-import { ProductDisplaySectionSkeleton, ProductTabSectionSkeleton } from "@/components";
+import {
+  ProductDisplaySectionSkeleton,
+  ProductTabSectionSkeleton,
+} from "@/components";
 
 const ShopIdPage = () => {
   const { shopId } = useParams();
   const { product, isLoading, isError } = useProductById({ id: shopId });
 
-  if (true)
+  if (isLoading)
     return (
       <div className="lg:space-y-6">
         <ProductDisplaySectionSkeleton />
         <ProductTabSectionSkeleton />
       </div>
     );
-  if (isLoading) return <h1>Loading...</h1>;
+
   if (isError || !product) return <div>Error loading product</div>;
 
   return (
     <div className="lg:space-y-6">
-      {/* <ProductDisplaySection product={product} />
-      <ProductTabSection product={product} /> */}
+      <ProductDisplaySection product={product} />
+      <ProductTabSection product={product} />
     </div>
   );
 };
